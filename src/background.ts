6dd72589e54cb5643efc24f284f0d495c9c1f940
Liyan Zhao <return65535@qq.com>
2024-04-23 01:35:25 +0800

    remove useless code

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,3 @@
-chrome.webRequest.onCompleted.addListener(
-    (details) => {
-        if (details.url.includes("yuketang.cn/api/v3/lesson/presentation/fetch")) {
-            details.requestId
-        }
-        const allSlides =
-            console.log(details);
-        return
-    },
-    { urls: ["*://*.yuketang.cn/*"], types: ["xmlhttprequest"] },
-    ["responseHeaders"]
-);
 chrome.runtime.onMessage.addListener((message: any, sender, sendResponse) => {
     if (message.type === 'getProblems') {
         chrome.storage.local.get('problems').then(p => console.log('request data:',p));
